f596c824894a15d4db331872f84a05afd3117572
figofer <figoferdian18@gmail.com>
2023-04-04 09:35:39 +0700

    menambahkan gambar pada tiap kategori

--- main.js
+++ main.js
@@ -4,21 +4,25 @@ function kalkulator() {
     const berat = Number(document.getElementById("berat").value)
     const rumus = tinggi / ((berat / 100) ** 2);
     let kategori;
+    let gambar;
     if (rumus < 18.5) {
-        kategori = "Underweight";
+        kategori = "Underweight ";
+        gambar = "<img src='/gambar/Underweight.png' alt='Underweight'>";
         document.getElementById("hasil").className = "Underweight";
     } else if (rumus < 24.9) {
         kategori = "Normal weight";
-        document.getElementById("hasil").className = "Underweight";
+        gambar = "<img src='/gambar/normal.png' alt='Normal'>";
+        document.getElementById("hasil").className = "Normal";
     } else if (rumus < 29.9) {
         kategori = "Overweight";
-        document.getElementById("hasil").className = "Underweight";
+        gambar = "<img src='/gambar/Overweight.png' alt='Overweight'>";
+        document.getElementById("hasil").className = "Overweight";
     } else {
         kategori = "Obesity";
-        document.getElementById("hasil").className = "Underweight";
+        gambar = "<img src='/gambar/Obesity.png'>";
+        document.getElementById("hasil").className = "Obesity";
     }
 
-    document.getElementById("hasil").innerHTML = 'Your BMI is <strong>' + rumus.toFixed(1) + ' </strong> which means You are <strong>' + kategori + '</strong>.';
+    document.getElementById("hasil").innerHTML = '<p>Your BMI is <b>' + rumus.toFixed(1) + ' </b> which means You are <b>' + kategori + '</b>.</p>' + gambar;
 
 }
-
